69607561d47f592e3612b4408566c06078c85ea4
Kai Widdeson <kwiddeson4@gmail.com>
2020-07-17 16:35:24 +0100

    Add materialUI

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello, World!</div>;
+  return (
+    <div>
+      <div className="App">Herllsdfo, World!</div>
+
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
+    </div>
+  );
 }
 
 export default App;
