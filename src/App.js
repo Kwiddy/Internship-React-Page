14ac5889335b5515db252c947ff7dcc68075934f
Kai Widdeson <kwiddeson4@gmail.com>
2020-07-19 10:31:36 +0100

    move links to stylesheets

--- src/App.js
+++ src/App.js
@@ -26,15 +26,6 @@ function App() {
           </div>
         </header>
       </div>
-
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-      />
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      />
     </div>
   );
 }
