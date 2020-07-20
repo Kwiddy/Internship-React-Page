e328a6a5ec431efa6c8c9de1b4b49be98235ce81
Kai <kwiddeson4@gmail.com>
2020-07-20 15:37:23 +0100

    improve button tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Adapter from "enzyme-adapter-react-16";
-import { render } from "@testing-library/react";
-import { shallow, configure } from "enzyme";
+import { render, wait } from "@testing-library/react";
+import { shallow, configure, mount } from "enzyme";
 import Button from "@material-ui/core/Button";
 import App from "./App";
 
@@ -14,12 +14,12 @@ test("Renders header", () => {
   expect(h1Element).toBeInTheDocument();
   expect(h2Element).toBeInTheDocument();
 });
-
 describe("Renders button-table buttons", () => {
   it("should be defined", () => {
     expect(Button).toBeDefined();
   });
-  it("should render cancelButton correctly", () => {
+  it("should render cancelButton", () => {
+    const mockFunction = jest.fn();
     const tree = shallow(<Button id="cancelButton" />);
     expect(tree).toMatchSnapshot();
   });
@@ -27,4 +27,5 @@ describe("Renders button-table buttons", () => {
     const tree = shallow(<Button id="updateButton" />);
     expect(tree).toMatchSnapshot();
   });
+  //Need to add tests for mockFunctions for onClick of buttons
 });
