a1764c55be4914431866f7b179b24c03c4379434
Kai <kwiddeson4@gmail.com>
2020-07-21 10:03:53 +0100

    Add true table data rendering from account object

--- src/accountDetails.js
+++ src/accountDetails.js
@@ -1,22 +1,50 @@
 import React from "react";
 
 class dettable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      details: [{ name: "John Doe", id: "ABCD1234" }],
+    };
+  }
+
+  renderTableData() {
+    return this.state.details.map((account, index) => {
+      const { name, id } = account;
+      return (
+        <tr key={index}>
+          <td>{name}</td>
+          <td>{id}</td>
+        </tr>
+      );
+    });
+  }
+
   render() {
     return (
-      <table id="details-table">
-        <tbody>
-          <tr>
-            <td>Account Name</td>
-            <td>John Doe</td>
-          </tr>
-          <tr>
-            <td>Account ID</td>
-            <td>ABC12345</td>
-          </tr>
-        </tbody>
-      </table>
+      <div>
+        <table id="details">
+          <tbody>{this.renderTableData()}</tbody>
+        </table>
+      </div>
     );
   }
+  // render() {
+  //   return (
+  //     <table id="details-table">
+  //       <tbody>
+  //         <tr>
+  //           <td>Account Name</td>
+  //           <td>John Doe</td>
+  //         </tr>
+  //         <tr>
+  //           <td>Account ID</td>
+  //           <td>ABC12345</td>
+  //         </tr>
+  //       </tbody>
+  //     </table>
+  //   );
+  // }
 }
 
 export default dettable;
