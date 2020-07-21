1dbe7d07ef3cfd326a824023c5570575973fa3ac
Kai <kwiddeson4@gmail.com>
2020-07-21 11:14:03 +0100

    test application renders

---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+jest.mock("react-dom");
+
+require("./index.js");
+
+test("Renders the application", () => {
+  expect(ReactDOM.render).toBeCalled();
+});
