06ab236e510e6f1387b8b5e83f6274257fa805cf
Kai <kwiddeson4@gmail.com>
2020-07-22 09:02:34 +0100

    remove test text

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
   return (
     <div className="App">
       <div className={classes.root}>
-        <Header mainText="You account HI" subText="Linked services HI" />
+        <Header mainText="You account" subText="Linked services" />
         <div className="page-wrapper">
           <div className="page-block">
             <Collapse in={open}>
